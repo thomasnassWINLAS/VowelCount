c7ea8ae24e3f877a97f1f72369218097d10ab6f4
thomasnass <thomas.nass@winlas.se>
2024-10-15 14:51:08 +0200

    VowelCount Kata

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,15 +1,33 @@
+using VowelCount;
+
 namespace Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+    //Return the number(count) of vowels in the given string.
+    //We will consider a, e, i, o, u as vowels for this Kata(but not y).
+    //The input string will only consist of lower case letters and/or spaces.
+
+    [Test]
+    public void GetVowelCount_Returns1_WhenGivenOneVowel()
     {
+        Assert.AreEqual(1, Kata.GetVowelCount("a"));
+    }
+    [Test]
+    public void GetVowelCount_Returns2_WhenGivenTwoVowel()
+    {
+        Assert.AreEqual(2, Kata.GetVowelCount("aa"));
+    }
+    [Test]
+    public void GetVowelCount_Returns0_WhenGivenNoVowels()
+    {
+        Assert.AreEqual(0, Kata.GetVowelCount("b"));
     }
 
     [Test]
-    public void Test1()
+    public void GetVowelCount_Returns5_WhenGivenTheAlphabet()
     {
-        Assert.Pass();
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        Assert.AreEqual(5, Kata.GetVowelCount(alphabet));
     }
 }
---
+++ VowelCount/Kata.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Linq;
+
+namespace VowelCount
+{
+    public class Kata
+    {
+        public static int GetVowelCount(string input)
+        {
+            return input.Count(c => new[] {'a','e','i','o','u' }.Contains(c));
+        }
+    }
+}
